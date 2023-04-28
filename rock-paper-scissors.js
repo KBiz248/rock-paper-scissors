f0a35f34c89d98a041bb042e42fa58adfa3c47ff
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-04-28 16:44:19 -0400

    Randomize computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,6 @@
 /* get computer choice and assign numeric value */
+let getComputerChoice = (Math.random() <= (1/3))? 1: (Math.random() <= (2/3))? 2: (Math.random() <= 1)? 3: "error";
+console.log(getComputerChoice)
 /* prompt user for choice and assign numeric value */
 /* perform function to determine the winner */
 /* display win/lose message and tally wins/losses */

eac066414f01ef5377ad461030c35ebd6cfa48e8
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-04-28 17:48:23 -0400

    Assign variables to determine if player wins/loses

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,11 +1,22 @@
 /* get computer choice and assign numeric value */
 let getComputerChoice = (Math.random() <= (1/3))? 1: (Math.random() <= (2/3))? 2: (Math.random() <= 1)? 3: "error";
+
 console.log(getComputerChoice)
+
 /* prompt user for choice and assign numeric value */
 let playerSelection = prompt("Rock, Paper, or Scissors?")
-let userChoice = (playerSelection.toLowerCase() == "rock")? 1: (playerSelection.toLowerCase() == "paper")? 2: (playerSelection.toLowerCase() == "scissors")? 3: "Try again";
+let playerChoice = (playerSelection.toLowerCase() == "rock")? 1: (playerSelection.toLowerCase() == "paper")? 2: (playerSelection.toLowerCase() == "scissors")? 3: "Try again";
+
 console.log(playerSelection)
-console.log(userChoice)
+console.log(playerChoice)
+
 /* perform function to determine the winner */
+let computerSelection = (getComputerChoice == 1)? "rock": (getComputerChoice == 2)? "paper": (getComputerChoice == 3)? "scissors": "error";
+let whoWon = (playerChoice - getComputerChoice == 1 || playerChoice - getComputerChoice == -2)? `You win! ${playerSelection} beats ${computerSelection}!` :  (playerChoice - getComputerChoice == 0)? `It's a tie!` : (playerChoice - getComputerChoice == -1 || playerChoice - getComputerChoice == 2)? `You lose! ${computerSelection} beats ${playerSelection}!`: "error";
+
+
+console.log(computerSelection)
+console.log(whoWon)
 
 /* display win/lose message and tally wins/losses */
+alert(whoWon);

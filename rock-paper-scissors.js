126e25c49d76d53f1626f3da7bfd04dc3992c6dd
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-04-28 17:30:26 -0400

    Code for player choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,5 +2,10 @@
 let getComputerChoice = (Math.random() <= (1/3))? 1: (Math.random() <= (2/3))? 2: (Math.random() <= 1)? 3: "error";
 console.log(getComputerChoice)
 /* prompt user for choice and assign numeric value */
+let playerSelection = prompt("Rock, Paper, or Scissors?")
+let userChoice = (playerSelection.toLowerCase() == "rock")? 1: (playerSelection.toLowerCase() == "paper")? 2: (playerSelection.toLowerCase() == "scissors")? 3: "Try again";
+console.log(playerSelection)
+console.log(userChoice)
 /* perform function to determine the winner */
+
 /* display win/lose message and tally wins/losses */

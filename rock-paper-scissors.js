6207aa9143688230f262172cb3a313eff7d08052
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-04-28 16:41:48 -0400

    Add psudocode to javascript sheet

---
+++ rock-paper-scissors.js
@@ -0,0 +1,4 @@
+/* get computer choice and assign numeric value */
+/* prompt user for choice and assign numeric value */
+/* perform function to determine the winner */
+/* display win/lose message and tally wins/losses */

c1d132cadcca421923c46783739d992a13d7488d
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-04-28 22:24:09 -0400

    Create for loop to run game five times

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,5 @@
+for (i = 0; i < 5; i++){
+
 /* get computer choice and assign numeric value */
 let getComputerChoice = (Math.random() <= (1/3))? 1: (Math.random() <= (2/3))? 2: (Math.random() <= 1)? 3: "error";
 
@@ -5,7 +7,7 @@ console.log(getComputerChoice)
 
 /* prompt user for choice and assign numeric value */
 let playerSelection = prompt("Rock, Paper, or Scissors?")
-let playerChoice = (playerSelection.toLowerCase() == "rock")? 1: (playerSelection.toLowerCase() == "paper")? 2: (playerSelection.toLowerCase() == "scissors")? 3: "Try again";
+let playerChoice = (playerSelection.toLowerCase() == "rock")? 1: (playerSelection.toLowerCase() == "paper")? 2: (playerSelection.toLowerCase() == "scissors")? 3: "error";
 
 console.log(playerSelection)
 console.log(playerChoice)
@@ -22,3 +24,6 @@ console.log(message)
 
 /* display win/lose message and tally wins/losses */
 alert(message);
+
+
+}

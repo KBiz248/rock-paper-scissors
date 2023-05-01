ed2327def5e60f55b3412b26197b85894f11d766
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-05-01 12:56:19 -0400

    Add exit message if player cancels

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,10 +3,10 @@ let playerWinCount = 0;
 let computerWinCount = 0;
 let tieCount = 0;
 
-for (i = 0; i < 5; i++){
+for (i = 0; i < 5; i++){{
 
 /* to play first to five wins (instead of just five rounds), comment out the above for loop and uncomment the below while loop */
-//while (playerWinCount < 5 && computerWinCount < 5){
+//while (playerWinCount < 5 && computerWinCount < 5){{
 
 /* get computer choice and assign numeric value */
 let getComputerChoice = (Math.random() <= (1/3))? 1: (Math.random() <= (2/3))? 2: (Math.random() <= 1)? 3: "error";
@@ -15,6 +15,10 @@ console.log(getComputerChoice)
 
 /* prompt user for choice and assign numeric value */
 let playerSelection = prompt("Rock, Paper, or Scissors?")
+
+/* displays exit message and breaks loop if player selects cancel */
+if (playerSelection == null){alert("Fine. I didn't want to play anyway."); break}
+
 let playerChoice = (playerSelection.toLowerCase() == "rock")? 1: (playerSelection.toLowerCase() == "paper")? 2: (playerSelection.toLowerCase() == "scissors")? 3: "error";
 
 console.log(playerSelection)
@@ -45,3 +49,4 @@ console.log(computerWinCount)
 let gameMessage = (playerWinCount > computerWinCount)? `You win! Final score: ${playerWinCount} to ${computerWinCount}` : (computerWinCount > playerWinCount)? `You lose! Final score: ${playerWinCount} to ${computerWinCount}` : `It's a tie! Final score: ${playerWinCount} to ${computerWinCount}`;
 
 alert(gameMessage);
+}
